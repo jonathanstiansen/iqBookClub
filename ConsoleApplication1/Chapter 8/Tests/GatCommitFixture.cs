05eec98285160db9ab2f070ea2044cd15c9f2524
jonathanstiansen <jonathan.stiansen@gmail.com>
2013-07-25 10:44:47 -0700

    Extracted Test Fixtures

---
+++ ConsoleApplication1/Chapter 8/Tests/GatCommitFixture.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using NUnit.Framework;
+
+namespace ConsoleApplication1.Chapter_9.Tests
+{
+    [TestFixture]
+    internal class GatCommitFixture : VersionControlActionBaseFixture
+    {
+       
+
+        [Test]
+        public void CommitShouldFailIfNotRun()
+        {
+            var commitAction = new GatCommitToVersionControl("20130924", "First Commit 001");
+
+            Assert.IsFalse(commitAction.Completed);
+        }
+
+        [Test]
+        public void CommitShouldPassIfExecutedWithMessageAndDate()
+        {
+            var commitAction = new GatCommitToVersionControl("20130924", "First Commit 001");
+            commitAction.Execute();
+
+            Assert.IsTrue(commitAction.Completed);
+        }
+
+        [Test]
+        public void CommitShouldReturnArrayWithLength()
+        {
+            var commitAction = new GatCommitToVersionControl("20130924", "First Commit 001");
+            ArrayList executionActions = commitAction.Execute();
+
+            Assert.IsTrue(executionActions.Count == 5);
+        }
+
+        [Test]
+        public void FailedCommitShouldHaveMessages()
+        {
+            var correctFailedMessages = new ArrayList
+                {
+                    "Logging",
+                    "Saving",
+                    "Running Gat Commit",
+                    "Action Failed",
+                    "Rolling Back",
+                    "State Restored"
+                };
+            var commitAction = new GatCommitToVersionControl("", "First Commit 001");
+            ArrayList executionMessages = commitAction.Execute();
+
+            Assert.IsTrue(haveSameStringsAtSameIndex(correctFailedMessages, executionMessages));
+        }
+
+        [Test]
+        public void FailedCommitShouldReturnArrayWithLength()
+        {
+            var commitAction = new GatCommitToVersionControl("", "First Commit 001");
+            ArrayList executionActions = commitAction.Execute();
+
+            Assert.IsTrue(executionActions.Count == 6);
+        }
+
+        [Test]
+        public void SuccessfulCommitShouldHaveMessages()
+        {
+            var correctSuccessMessages = new ArrayList
+                {
+                    "Logging",
+                    "Saving",
+                    "Running Gat Commit",
+                    "Action Successful",
+                    "Action Completed"
+                };
+            var commitAction = new GatCommitToVersionControl("20130924", "First Commit 001");
+            ArrayList executionMessages = commitAction.Execute();
+
+            Assert.IsTrue(haveSameStringsAtSameIndex(correctSuccessMessages, executionMessages));
+        }
+
+    }
+}
---
+++ ConsoleApplication1/Chapter 8/Tests/VersionControlActionBaseFixture.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+
+namespace ConsoleApplication1.Chapter_9.Tests
+{
+    public class VersionControlActionBaseFixture
+    {
+        // Helper Method
+        protected bool haveSameStringsAtSameIndex(ArrayList list1, ArrayList list2)
+        {
+            bool haveSameElements = list1.Count == list2.Count;
+            for (int i = 0; i < list1.Count; ++i)
+            {
+                bool sameItemAtIndexI = list1.IndexOf(i) == list2.IndexOf(i);
+                haveSameElements = haveSameElements && sameItemAtIndexI;
+            }
+            return haveSameElements;
+        }
+    }
+}
